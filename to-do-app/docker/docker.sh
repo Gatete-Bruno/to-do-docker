eb831f1374067e295b44c0a615ff3c2d7a0540a7
GATETE KATO BRUNO <64279605+Gatete-Bruno@users.noreply.github.com>
2024-02-03 11:58:27 +0200

    I am Batman

--- to-do-app/docker/docker.sh
+++ to-do-app/docker/docker.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install docker.io -y
@@ -8,3 +9,10 @@ sudo chmod 777 /var/run/docker.sock
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Enable and start Docker service
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Check Docker service status
+sudo systemctl status docker

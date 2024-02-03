45688db80fb69535a104d66d67e7ed4f81f68b92
GATETE KATO BRUNO <64279605+Gatete-Bruno@users.noreply.github.com>
2024-02-03 11:48:27 +0200

    I am Batman

--- to-do-app/docker/docker.sh
+++ to-do-app/docker/docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install docker.io -y
-sudo usermod -aG docker kato  #my case is kato 
+sudo usermod -aG docker kato  # Assuming the user is 'kato'
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
